202b3aff56909534ea481cf7ddf19f8196c547a1
Fondant13200 <guillaume.perez@imerir.com>
2015-10-07 18:01:20 +0200

    Finalisation du code
    Création d'un dossier nommé Projet contenant les différentes classes
    pour exécuter le programme permettant au robot de communiquer avec le
    serveur et de dessiner sur la feuille.

---
+++ trunk/Robot_Kuka/Projet/Vector4.java
@@ -0,0 +1,27 @@
+package application;
+
+// Cette classe permet de me stocker 2 points, permettant ainsi de créer une ligne facilement
+public class Vector4 {
+
+		public double x;
+		public double y;
+		public double a;
+		public double b;
+		
+		public Vector4(){
+			x = 0;
+			y = 0;
+			a = 0;
+			b = 0;
+		}
+
+		public Vector4(double x2, double y2, double x3, double y3) {
+			// TODO Auto-generated constructor stub
+			x = x2;
+			y = y2;
+			a = x3;
+			b = y3;
+		}
+		
+	}
+
